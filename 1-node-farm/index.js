c1ff599828c501c0d9a285aae50c5e6566b26967
jaiswalshiva <jaiswal.shivam@tftus.com>
2022-03-02 07:47:47 +0530

    first message

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -44,8 +44,24 @@ const url = require('url');
 //   `${__dirname}/templates/template-product.html`,
 //   'utf-8'
 // );
+const replaceTemplate = (temp,product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+  output = output.replace(/{%IMAGE%}/g,product.image);
+  output = output.replace(/{%PRICE%}/g,product.price);
+  output = output.replace(/{%FROM%}/g,product.from);
+  output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g,product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g,product.description);
+  output = output.replace(/{%ID%}/g,product.id);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+  if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+  
+}
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`,'utf-8');
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
 
@@ -54,19 +70,25 @@ const server = http.createServer((req, res) => {
 
   // Overview page
   if (pathname === '/' || pathname === '/overview') {
-    res.end('This is the overview');
+    res.writeHead(200,{'Content-type':'text/html'});
+  const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+  const output = tempOverview.replace('{%PRODUCT_CARDS%}' ,cardsHtml)
+  
+    res.end(output);
     }
-    else if (pathname === '/product') {
-    res.end('This is the PRODUCT');
+    //product page 
+    else if (pathname === '/product') { 
+      res.writeHead(200,{'Content-type':'text/html'});
+      const product = dataObj[query.id];
+      const output = replaceTemplate(tempProduct , product);
+     res.end(output);
     }
+    //API
     else if (pathname === '/api') {
-    fs.readFile(`{__dirname}dev-data/data.json` , 'utf-8',(err , data)=>{
-      const productData = JSON.parse(data);
-      console.log(productData);
-    })
-    res.end('API');
-    }
-     else{
+       res.writeHead(200,{'Content-type':'application/json'});
+       res.end(data);
+    //not found 
+   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world'
