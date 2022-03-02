9c87d6ee50fc1bca418cdb86c86751df5f7bd720
jaiswalshiva <jaiswal.shivam@tftus.com>
2022-03-02 13:23:49 +0530

    first message

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify')
+const replaceTemplate = require('./starter/modules/replaceTemplate');
 
 
 /////////////////////////////////
@@ -44,26 +46,15 @@ const url = require('url');
 //   `${__dirname}/templates/template-product.html`,
 //   'utf-8'
 // );
-const replaceTemplate = (temp,product)=>{
-  let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-  output = output.replace(/{%IMAGE%}/g,product.image);
-  output = output.replace(/{%PRICE%}/g,product.price);
-  output = output.replace(/{%FROM%}/g,product.from);
-  output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g,product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g,product.description);
-  output = output.replace(/{%ID%}/g,product.id);
 
-  if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-  
-}
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`,'utf-8');
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
